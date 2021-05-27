a82505c2e21e41712f8c622482c1fe59b4b3c47b
Lars van Rhijn <larsvanrhijn@gmail.com>
2021-05-27 21:50:01 +0200

    Documentation

--- backend/website/games/models.py
+++ backend/website/games/models.py
@@ -7,6 +7,10 @@ class Item(models.Model):
 
     name = models.CharField(max_length=1024, help_text="The name of the item")
 
+    def __str__(self):
+        """Convert this object to string."""
+        return self.name
+
 
 class DrinkingGame(models.Model):
     """Drinking Game model."""
@@ -16,4 +20,5 @@ class DrinkingGame(models.Model):
     items = models.ManyToManyField(Item, help_text="The items needed to play this drinking game.", blank=True)
 
     def __str__(self):
+        """Convert this object to string."""
         return self.name
