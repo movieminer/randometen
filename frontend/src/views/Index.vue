a53d57c2082b5209fec37189060677920e3b706d
Lars van Rhijn <larsvanrhijn@gmail.com>
2021-05-28 13:06:22 +0200

    Credits

--- frontend/src/views/Index.vue
+++ frontend/src/views/Index.vue
@@ -25,6 +25,9 @@
       <span v-on:click="clickedScreen($event)" class="text-black uppercase text-2xl font-primary cursor-pointer">{{ games[currentGameIndex].name }}</span>
     </div>
   </div>
+  <footer v-if="!playing && currentGame === null" class="fixed bottom-0 my-2 w-full block flex justify-center">
+    <a href="https://github.com/KiOui/drankspel" class="underline opacity-80 mx-auto text-black">Check the source code on GitHub</a>
+  </footer>
 </template>
 
 <script lang="ts">
